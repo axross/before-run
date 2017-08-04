ce0741bf89e582157ffa97177a365638968848b0
axross <me@axross.io>
2017-08-05 00:02:42 +0900

    Add the stringtoUint8List utility function

---
+++ lib/src/utility/string_to_uint8list.dart
@@ -0,0 +1,16 @@
+import 'dart:convert' show UTF8;
+import 'dart:typed_data' show Uint8List;
+
+final _padding = [
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+Uint8List stringToUint8List(String value) =>
+  new Uint8List.fromList(
+    (UTF8.encode(value).toList()
+      ..addAll(_padding)
+    ).sublist(0, 32),
+  );
