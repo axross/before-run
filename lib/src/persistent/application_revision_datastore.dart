647cdbdb10901557fa825cb7dc5b32da93238456
axross <me@axross.io>
2017-08-11 14:48:04 +0900

    Add getById in the Application revision datastore

--- lib/src/persistent/application_revision_datastore.dart
+++ lib/src/persistent/application_revision_datastore.dart
@@ -16,6 +16,18 @@ class ApplicationRevisionDatastore {
     return rows.map<ApplicationRevision>((row) => deserializeToApplicationRevision(row));
   }
 
+  Future<ApplicationRevision> getById(Connection connection, {@required Uuid id, @required User requester}) async {
+    final rows = await connection.query('select id, application_id, creator_id, created_at from application_revisions where id = @id limit 1;', {
+      'id': '$id',
+    }).toList();
+
+    if (rows.length == 0) {
+      throw new ApplicationRevisionNotFoundException(id: id);
+    }
+
+    return deserializeToApplicationRevision(rows.single);
+  }
+
   Future<ApplicationRevision> create(Connection connection, {@required Application application, @required User requester}) async {
     final row = await connection.query('insert into application_revisions (id, application_id, creator_id, created_at) values (@id, @applicationId, @creatorId, @now) returning id, application_id, creator_id, created_at;', {
       'id': new Uuid.v4().toString(),
